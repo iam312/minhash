f68eb6abf369d235c6f994c63494160854c9b149
sammy <sammy@kakao.com>
2014-07-04 20:05:36 +0900

    모듈 include 디렉토리를 현재디렉토리로 변경.

--- lib/mapper.py
+++ lib/mapper.py
@@ -2,28 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import sys;
-import lib.seeds;
-import lib.minhash;
+import seeds;
+import minhash;
 
 def _mapper(fields, seeds, funcs):
     key = fields[0]
     values = fields[1].split(",") # value 는 ',' 로 구분되어 있다.
     
-    for value in lib.minhash.minhash(values, seeds, funcs) :
+    for value in minhash.minhash(values, seeds, funcs) :
         print("%s\t%s" % (value, key))
         
 
 def main(argv):
     with open('seed.txt') as f:
         content = f.readlines()
-        seeds, funcs = lib.seeds.load(content)
+        raw_seeds, funcs = seeds.load(content)
 
     line = sys.stdin.readline();
     try:
         while line:
             line = line[:-1];
             fields = line.split(":");
-            _mapper(fields, seeds, funcs);
+            _mapper(fields, raw_seeds, funcs);
             line = sys.stdin.readline();
     except "end of file":
         return None
